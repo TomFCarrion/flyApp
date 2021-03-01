f2e50b34c0bff4154c428341ee02b1a1252d3622
tomfcarrion <tomfcarrion@gmail.com>
2021-03-01 10:34:32 -0300

    feat: general structure

---
+++ src/components/Result/Result.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Result= () => {
+    
+    return (
+        <h1>Search Your Flight </h1>
+    );
+};
+export default Result;
+
---
+++ src/components/Result/ResultWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ResultWrapper = () => {
+    
+    return (
+        <h1>Search Your Flight </h1>
+    );
+};
+export default ResultWrapper;
+
