6efefdc222ca060ae5855150c54d182c4136e116
tomfcarrion <tomfcarrion@gmail.com>
2021-03-02 09:39:45 -0300

    feat: add appWrapper

---
+++ src/components/AppWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchWrapper from './Search/SearchWrapper'
+import './appWrapper.scss'
+
+
+const AppWrapper = () => (
+
+     <SearchWrapper/>
+);
+
+export default AppWrapper;
+
