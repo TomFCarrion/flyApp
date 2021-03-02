b509c91fce4f3cada1a5284dbc4e1e6e887f57f1
tomfcarrion <tomfcarrion@gmail.com>
2021-03-02 09:38:38 -0300

    config: webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld'
+import { Provider } from 'react-redux';
+import  App  from './routes/App';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers/index';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>
+    , document.getElementById('root')
+  );
