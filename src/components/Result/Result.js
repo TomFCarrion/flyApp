d75a82266cc9a2b4332736dfb1a527ca3fac2e87
tomfcarrion <tomfcarrion@gmail.com>
2021-03-02 21:07:44 -0300

    feat: add result

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import './result.scss'
+import moment from 'moment';
 
 
-const Result= () => {
-    
+
+const Result = ({from = "", to="", airlineName = "-",flightNumber = "0000", ticketPrice = "-", date="-"}) => {
+    const momentDate = moment(date).format('MMM-DD');
     return (
-        <h1>Search Your Flight </h1>
+
+        <div className="Card">
+            <div className="upper">
+                <span className="upper-item">{from}</span>
+                <span className="upper-item airline">{airlineName}</span>
+                <span className="upper-item">{to}</span>
+
+            </div>
+            <div className="middle">
+                <div>Depart</div>
+                <div>{momentDate}</div>
+                <span>{flightNumber}</span>
+            </div>
+            <div className="bottom">
+            <span className="price"> {`$ ${ticketPrice}`}</span>
+            </div>
+
+
+        </div>
+        
     );
 };
 export default Result;
