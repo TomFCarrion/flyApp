92198a3d4fca140f31d1dd79b2418413643c1d8f
tomfcarrion <tomfcarrion@gmail.com>
2021-03-04 12:24:19 -0300

    config: heroku config

--- src/server/server.js
+++ src/server/server.js
@@ -76,7 +76,7 @@ app.get('*', renderApp);
 
 
 
-app.listen(PORT, (err) => {
+app.listen(PORT || 3000, (err) => {
     if (err) console.log(err);
     else console.log(`Server running on port ${PORT}`);
   });
